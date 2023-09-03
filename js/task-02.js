3031e49330054f921af289114d55967018a1b288
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-03 18:02:31 +0300

    task 4

--- js/task-02.js
+++ js/task-02.js
@@ -11,7 +11,9 @@ const ingredientsList = document.querySelector("#ingredients");
 const liElements = ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
-  li.classList.add("item", "data-item");
+  li.classList.add("item");
   return li;
 });
 ingredientsList.append(...liElements);
+
+console.log(liElements);
