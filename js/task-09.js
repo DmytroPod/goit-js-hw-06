0579477093400e1af2b655dbcee518df0fecc12d
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-04 20:11:24 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const hexSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", makerColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
+}
+
+function makerColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  hexSpan.textContent = color;
 }
