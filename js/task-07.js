223544167348132aec27f331e4cd41145c0c2d22
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-05 20:02:16 +0300

    13

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,18 @@
-const curentFontSize = document.querySelector("#font-size-control");
-const textSpan = document.querySelector("#text");
-textSpan.style.fontSize = "56px";
-curentFontSize.addEventListener("input", handleFontSize);
+// const curentFontSize = document.querySelector("#font-size-control");
+// const textSpan = document.querySelector("#text");
+// textSpan.style.fontSize = "56px";
+// curentFontSize.addEventListener("input", handleFontSize);
 
-function handleFontSize(evt) {
-  const fontSize = evt.target.value + "px";
-  textSpan.style.fontSize = fontSize;
-}
+// function handleFontSize(evt) {
+//   const fontSize = evt.target.value + "px";
+//   textSpan.style.fontSize = fontSize;
+// }
+const refs = {
+  curentFontSize: document.querySelector("#font-size-control"),
+  textSpan: document.querySelector("#text"),
+};
+refs.textSpan.style.fontSize = `${refs.curentFontSize.value}px`;
+
+refs.curentFontSize.addEventListener("input", () => {
+  refs.textSpan.style.fontSize = `${refs.curentFontSize.value}px`;
+});
