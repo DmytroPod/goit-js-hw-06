f397be4158bd0e5354016637037f9442fe46d1a8
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-09-05 15:54:35 +0300

    fin

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,9 @@
 const curentFontSize = document.querySelector("#font-size-control");
 const textSpan = document.querySelector("#text");
-
+textSpan.style.fontSize = "56px";
 curentFontSize.addEventListener("input", handleFontSize);
 
 function handleFontSize(evt) {
   const fontSize = evt.target.value + "px";
-  console.log(fontSize);
   textSpan.style.fontSize = fontSize;
-  textSpan.textContent = fontSize;
 }
